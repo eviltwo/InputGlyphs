2b13e52e5cafcc9d2f11fe1aae4a495b8f9c2efe
DAIKI <daiki.evilone@gmail.com>
2024-07-05 01:39:03 +0900

    fix test namespace

--- InputGlyphs/Assets/InputGlyphsTests/Utils/InputLayoutPathUtilityTest.cs
+++ InputGlyphs/Assets/InputGlyphsTests/Utils/InputLayoutPathUtilityTest.cs
@@ -1,16 +1,19 @@
 using InputGlyphs.Utils;
 using NUnit.Framework;
 
-public class InputLayoutPathUtilityTest
+namespace InputGlyphs.Tests
 {
-    [TestCase("XInputController/buttonSouth", "buttonSouth")]
-    [TestCase("XInputController/dpad/left", "dpad/left")]
-    [TestCase("<gamepad>/dpad/up", "dpad/up")]
-    [TestCase("<gamepad>", "")]
-    [TestCase("<gamepad>/", "")]
-    public void GetLocalPath(string input, string expected)
+    public class InputLayoutPathUtilityTest
     {
-        var result = InputLayoutPathUtility.GetLocalPath(input);
-        Assert.AreEqual(expected, result);
+        [TestCase("XInputController/buttonSouth", "buttonSouth")]
+        [TestCase("XInputController/dpad/left", "dpad/left")]
+        [TestCase("<gamepad>/dpad/up", "dpad/up")]
+        [TestCase("<gamepad>", "")]
+        [TestCase("<gamepad>/", "")]
+        public void GetLocalPath(string input, string expected)
+        {
+            var result = InputLayoutPathUtility.GetLocalPath(input);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
