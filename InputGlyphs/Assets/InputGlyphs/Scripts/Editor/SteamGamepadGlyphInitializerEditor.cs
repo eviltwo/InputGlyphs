eef812bd30d0a38f424807217081a36b53927c89
DAIKI <daiki.evilone@gmail.com>
2024-07-07 21:37:44 +0900

    Add package version to import url

--- InputGlyphs/Assets/InputGlyphs/Scripts/Editor/SteamGamepadGlyphInitializerEditor.cs
+++ InputGlyphs/Assets/InputGlyphs/Scripts/Editor/SteamGamepadGlyphInitializerEditor.cs
@@ -8,76 +8,99 @@ namespace InputGlyphs.Loaders.Steam.Editor
     [CustomEditor(typeof(SteamGamepadGlyphInitializer))]
     public class SteamGamepadGlyphInitializerEditor : UnityEditor.Editor
     {
-        private bool _foldoutSteamworks;
-        private bool _foldoutAdapter;
-        private bool _foldoutLoader;
+        private class RequiredPackageInfo
+        {
+            public string title;
+            public string name;
+            public string pageUrl;
+            public string packageUrl;
+            public string requiredVersion;
+            public bool isInstalled;
+            public double installTime;
+            public bool isFoldout;
+        }
+
+        private RequiredPackageInfo[] _requiredPackages = new RequiredPackageInfo[]
+        {
+            new RequiredPackageInfo
+            {
+                title = "Steamworks.NET",
+                name = "com.rlabrecque.steamworks.net",
+                pageUrl = "https://github.com/rlabrecque/Steamworks.NET",
+                packageUrl = "https://github.com/rlabrecque/Steamworks.NET.git?path=/com.rlabrecque.steamworks.net",
+                requiredVersion = "20.2.0",
+#if SUPPORT_STEAMWORKS
+                isInstalled = true,
+#endif
+            },
+            new RequiredPackageInfo
+            {
+                title = "Steam Input Adapter",
+                name = "com.eviltwo.unity-steam-input-adapter",
+                pageUrl = "https://github.com/eviltwo/UnitySteamInputAdapter",
+                packageUrl = "https://github.com/eviltwo/UnitySteamInputAdapter.git?path=UnitySteamInputAdapter/Assets/UnitySteamInputAdapter",
+                requiredVersion = "0.9.2",
+#if SUPPORT_ADAPTER
+                isInstalled = true,
+#endif
+            },
+            new RequiredPackageInfo
+            {
+                title = "Steam Input Glyph Loader",
+                name = "com.eviltwo.unity-steam-input-glyph-loader",
+                pageUrl = "https://github.com/eviltwo/UnitySteamInputGlyphLoader",
+                packageUrl = "https://github.com/eviltwo/UnitySteamInputGlyphLoader.git?path=UnitySteamInputGlyphLoader/Assets/UnitySteamInputGlyphLoader",
+                requiredVersion = "0.10.0",
+#if SUPPORT_LOADER
+                isInstalled = true
+#endif
+            }
+        };
+
         private Dictionary<string, string> _versionCache = new Dictionary<string, string>();
 
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
 
-            var requirePackages = 0;
             var installedPackages = 0;
-
-            var isSteamworksInstalled = false;
-            requirePackages++;
-#if SUPPORT_STEAMWORKS
-            isSteamworksInstalled = true;
-            installedPackages++;
-#endif
-
-            var isAdapterInstalled = false;
-            requirePackages++;
-#if SUPPORT_ADAPTER
-            isAdapterInstalled = true;
-            installedPackages++;
-#endif
-
-            var isLoaderInstalled = false;
-            requirePackages++;
-#if SUPPORT_LOADER
-            isLoaderInstalled = true;
-            installedPackages++;
-#endif
-
+            for (var i = 0; i < _requiredPackages.Length; i++)
+            {
+                if (_requiredPackages[i].isInstalled)
+                {
+                    installedPackages++;
+                }
+            }
+            var requirePackages = _requiredPackages.Length;
             var labelStyle = new GUIStyle(EditorStyles.label);
             labelStyle.richText = true;
             var packageCompleted = installedPackages == requirePackages;
             var labelTitle = packageCompleted ? "All packages are installed." : $"Packages installed: {installedPackages}/{requirePackages}";
             EditorGUILayout.LabelField(labelTitle, labelStyle);
 
-            DrawPackageInfo(
-                "Steamworks.NET",
-                GetPackageVersion("com.rlabrecque.steamworks.net"),
-                isSteamworksInstalled,
-                "https://github.com/rlabrecque/Steamworks.NET",
-                "https://github.com/rlabrecque/Steamworks.NET.git?path=/com.rlabrecque.steamworks.net",
-                ref _foldoutSteamworks);
-
-            DrawPackageInfo(
-                "SteamInputAdapter",
-                GetPackageVersion("com.eviltwo.unity-steam-input-adapter"),
-                isAdapterInstalled,
-                "https://github.com/eviltwo/UnitySteamInputAdapter",
-                "https://github.com/eviltwo/UnitySteamInputAdapter.git?path=UnitySteamInputAdapter/Assets/UnitySteamInputAdapter",
-                ref _foldoutAdapter);
-
-            DrawPackageInfo(
-                "SteamInputGlyphLoader",
-                GetPackageVersion("com.eviltwo.unity-steam-input-glyph-loader"),
-                isLoaderInstalled,
-                "https://github.com/eviltwo/UnitySteamInputGlyphLoader",
-                "https://github.com/eviltwo/UnitySteamInputGlyphLoader.git?path=UnitySteamInputGlyphLoader/Assets/UnitySteamInputGlyphLoader",
-                ref _foldoutLoader);
+            for (var i = 0; i < _requiredPackages.Length; i++)
+            {
+                var requiredPackage = _requiredPackages[i];
+                DrawPackageInfo(
+                    requiredPackage.title,
+                    GetPackageVersion(requiredPackage.name),
+                    requiredPackage.requiredVersion,
+                    requiredPackage.pageUrl,
+                    requiredPackage.packageUrl,
+                    requiredPackage.isInstalled,
+                    ref requiredPackage.installTime,
+                    ref requiredPackage.isFoldout);
+            }
         }
 
         private static void DrawPackageInfo(
             string packageTitle,
             string installedPackageVersion,
-            bool installed,
+            string requiredPackageVersion,
             string packagePageUrl,
             string packageUrl,
+            bool installed,
+            ref double installTime,
             ref bool foldout)
         {
             var foldoutStyle = new GUIStyle(EditorStyles.foldoutHeader);
@@ -91,9 +114,15 @@ namespace InputGlyphs.Loaders.Steam.Editor
                     Application.OpenURL(packagePageUrl);
                 }
                 EditorGUILayout.LabelField($"Installed version : {installedPackageVersion}");
-                if (GUILayout.Button("Import package using Unity Package Manager"))
+                EditorGUILayout.LabelField($"Required version : {requiredPackageVersion}");
+                if (EditorApplication.timeSinceStartup < installTime + 5.0)
                 {
-                    Client.Add(packageUrl);
+                    EditorGUILayout.LabelField("Installing...");
+                }
+                else if (GUILayout.Button("Import package using Package Manager"))
+                {
+                    installTime = EditorApplication.timeSinceStartup;
+                    Client.Add($"{packageUrl}#{requiredPackageVersion}");
                 }
             }
             EditorGUILayout.EndFoldoutHeaderGroup();
